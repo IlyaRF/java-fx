0496340eb2da62db87df031726f5811ee49b0e96
IlyaRF <ilyagaitanov@gmail.com>
2022-08-24 20:17:02 +0300

    ДЗ сетевой чат

--- src/main/java/com/example/javachatwindow/server/Connection.java
+++ src/main/java/com/example/javachatwindow/server/Connection.java
@@ -1,61 +1,52 @@
 package com.example.javachatwindow.server;
 
 import java.io.IOException;
+import java.sql.*;
 
 public class Connection implements AuthService {
 
 
     public static final String DB_PATH = "src/main/resources/SqlLite.db";
+    private static Statement stmt;
+    private static java.sql.Connection connection;
 
-    private static Connection connection;
-
-    public static void main(String[] args) throws IOException {
+    public Connection() {
         try {
-            connect();
-        } catch (Exception e) {
-            e.printStackTrace();
-        } finally {
-            disconnect();
+            connection = DriverManager.getConnection("jdbc:sqlite" + DB_PATH);
+        } catch (SQLException e) {
+           throw new RuntimeException("Не удалось подключиться к базе " + e.getMessage(), e);
         }
-    }
-
-    public static void connect() {
-        connection = DriverManager.getConnection("jdbc:sqlite" + DB_PATH);
     }
 
     @Override
     public String getNickByLoginAndPassword(String login, String password) {
        try {
-           PreparedStatement stmt = connection.prepareStatement("select username from auth where login = ? and password = ?");
-           stmt.setString = (1, login);
-           stmt.setString = (2, password);
+           stmt = connection.prepareStatement("select username from auth where login = ? and password = ?");
+           stmt.setString (1, login);
+           stmt.setString (2, password);
 
            ResultSet resultSet = stmt.executeQuery();
 
            return resultSet.getString(1);
        }catch (SQLException e) {
-           System.out.println("Не удалосб получить USER NAME " + e.getMessage());
+           System.out.println("Не удалось получить USER NAME " + e.getMessage());
            return null;
        }
+    }
+
+    public static void disconnect() {
+        try {
+            if (connection != null) {
+                connection.close();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 
     @Override
     public void close() throws IOException {
 
-    }
-
-    static class SQLException extends Exception {
-    }
-
-    private static class DriverManager {
-        public static Connection getConnection(String s) {
-            return null;
-        }
-    }
-
-    public static void disconnect() throws IOException {
-        if (connection != null) {
-            connection.close();
-        }
     }
 }
